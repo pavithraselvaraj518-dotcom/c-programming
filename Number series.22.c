4974293ae9a6c7f810bbfebce9d34ac6bef2544e
pavithraselvaraj518-dotcom <pavithraselvaraj518@gmail.com>
2026-07-20 11:41:35 +0530

    Add salary classification based on input value

---
+++ Number series.22.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+  int salary;
+scanf("%d", &salary);
+if(salary > 50000)
+  printf("High Salary");
+else if(salary >= 25000)
+  printf("Medium Salary");
+else
+  printf("Low Salary");
+return 0;
+}
+    
+
