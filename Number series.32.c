c15ed33558ab35bef20f515d695aba3a544fef2d
pavithraselvaraj518-dotcom <pavithraselvaraj518@gmail.com>
2026-07-21 21:25:13 +0530

    Add number comparison program in Number series.32.c

---
+++ Number series.32.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+  int s;
+scanf("%d", &s);
+if(s>b)
+  printf(s+" it is Largest");
+else if(s<b)
+  printf(s+" it is Not Largest");
+else
+  printf(s+" it is Equal");
+return 0;
+}
