884f65a8deb65f94a5d981a69bf249b3ed808ded
pavithraselvaraj518-dotcom <pavithraselvaraj518@gmail.com>
2026-07-23 20:14:10 +0530

    Implement delivery fee check based on amount
    Add a program to check free delivery based on purchase amount.

---
+++ Number series.43.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  int amount;
+scanf("%d", &amount);
+printf("Enter Purchase Amount");
+if (amount >=500)
+{
+printf("Free Delivary");
+}
+else
+{
+printf("Not Free Delivary")
+}
+return 0;
+}
+
